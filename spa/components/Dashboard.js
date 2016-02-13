e285430d05fd0f25acc6b0e5443bfed89fe1a308
Harald Held <harald.held@gmail.com>
2016-02-13 13:51:29 +0100

    Showing gravatar for a logged-in user.

--- spa/components/Dashboard.js
+++ spa/components/Dashboard.js
@@ -1,12 +1,17 @@
 import React, { Component, PropTypes } from 'react';
+import md5 from 'md5';
 
 class Dashboard extends Component {
     render() {
-        const {userName, realName} = this.props.userInfo;
+        const {userName, realName, email} = this.props.userInfo;
+        const emailHash = md5(email);
+        const gravatarUrl = 'https://secure.gravatar.com/avatar/' + emailHash;
+
         return (
             <div>
                 <h1>Dashboard</h1>
                 <p>Welcome {realName} ({userName})</p>
+                <img src={gravatarUrl} />
             </div>
         );
     }
