23f97f9e1a64a279e916f0821db5b00495916bc6
Harald Held <harald.held@gmail.com>
2016-02-21 11:24:11 +0100

    Enabled support for Chrome debugger extension for redux.

--- spa/store/configureStore.js
+++ spa/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import { syncHistory } from 'react-router-redux';
@@ -7,10 +7,10 @@ import createBrowserHistory from 'history/lib/createBrowserHistory';
 export const browserHistory = createBrowserHistory();
 const reduxRouterMiddleware = syncHistory(browserHistory);
 
-const createStoreWithMiddleware = applyMiddleware(
+const createStoreWithMiddleware = compose(applyMiddleware(
     thunk,
     reduxRouterMiddleware
-)(createStore);
+), window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore);
 
 export default function configureStore(initialState) {
     return createStoreWithMiddleware(rootReducer, initialState);
